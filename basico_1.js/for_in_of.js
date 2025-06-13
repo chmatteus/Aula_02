69b5d15e49e003ac826711554cedda2e334c36b7
chmatteus <matteus_fr@hotmail.com>
2025-06-12 21:14:43 -0300

    arrays em js

---
+++ basico_1.js/for_in_of.js
@@ -0,0 +1,19 @@
+const notas = [7, 9, 6, 10, 5];
+
+
+let valores = '';
+
+for (nota of notas) {
+    valores += nota + ' / ';
+}
+
+console.log(valores);
+
+let indices = '';
+
+for (nota in notas) {
+    indices += notas + ' - ';
+}
+
+console.log(indices); 
+
